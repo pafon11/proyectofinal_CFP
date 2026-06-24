93ba85e4798ea7742cb2c5b9fff8976f291605cb
pafon11 <cuevas.fonseca1103@gmail.com>
2026-06-24 17:21:33 -0600

    increase timeout and reduce forest estimators

--- models/forest_covid.py
+++ models/forest_covid.py
@@ -58,7 +58,7 @@ def entrenar_y_guardar():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     print("Entrenando Random Forest...")
-    sabios = RandomForestClassifier(n_estimators=100, random_state=42, n_jobs=-1)
+    sabios = RandomForestClassifier(n_estimators=50, random_state=42, n_jobs=-1)
     sabios.fit(X_train, y_train)
     print("   Entrenamiento completado!")
 
